4d9609247cb2b5b07f5b84591d4f3d1de2ba4bb1
Batriftale <70813542+Batriftale@users.noreply.github.com>
2020-10-16 15:30:27 +0300

    Update task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+#1
 class Condensator{
     private:
         int m_C;
@@ -22,5 +23,34 @@ int main(){
     cin >> c >> u;
     Condensator cond(c, u);
     cout << "Capacitor's energy is " <<  cond.energy();
+
+}
+#2
+class Product{
+    private:
+        string m_name;
+        int m_quant;
+        double m_price;
+    public:
+        Product(string name, int quantity, double price){
+            m_quant = quantity;
+            m_price = price;
+            m_name = name;
+        }
+        void summ(){
+            double s = m_quant * m_price;
+            cout << m_name << " is " << s << " in total" << endl;
+           };
+};
+int main(){
+    int q;
+    double p;
+    string name;
+
+    cout << "Type name, quantity and price of a product " << endl;
+    cin >> name >> q >> p;
+
+    Product prod(name, q, p);
+    prod.summ();
 
 }
